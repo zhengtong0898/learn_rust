705a2fb360b7b28244809ac59a683043953efd9f
zhengtong0898 <zhengtong0898@aliyun.com>
2023-11-27 22:33:28 +0800

    更新 world_hello 项目, 通过 Instant::now 度量代码耗时情况

--- world_hello/src/main.rs
+++ world_hello/src/main.rs
@@ -1,8 +1,15 @@
+use std::time::Instant;
+
 #[allow(unused_variables)]
 fn main() {
-    let x = 5;
-    let equal_to_x = || println!("{}", x);
-    println!("{}", x);
-    equal_to_x();
-    println!("{}", x);
+    let start_time = Instant::now(); // 耗时: < 100 nanos
+    let x = 5; //                           耗时: < 100 nanos
+    let s = 6; //                           耗时: < 100 nanos
+    let ss = "good"; //                    耗时: < 100 nanos
+    let sss = "hello world!".to_string(); //                                               耗时: debug:    500 nanos; release:    200 nanos;
+    let duration = Instant::now().duration_since(start_time); // 前面5行代码加起来耗时: debug:    700 nanos; release:    100 nanos;
+    println!("assign variable: {}", duration.as_nanos()); //                                这行代码耗时: debug: 110,000 nano; release: 53,000 nanos;
+
+    let duration = Instant::now().duration_since(start_time); //          这行代码耗时: debug:    100 nanos; release:    100 nanos;
+    println!("assign variable: {}", duration.as_nanos());
 }
